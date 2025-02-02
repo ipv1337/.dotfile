95336092b31e7e3d570b32ef3f396729790f404f
James H. Nguyen <james.nguyen@flyr.com>
2025-02-01 23:32:09 -0800

    Update dotfile exports

--- .oh-my-zsh/custom/exports.zsh
+++ .oh-my-zsh/custom/exports.zsh
@@ -4,4 +4,8 @@ export EDITOR='vim'
 
 export JAVA_HOME="$(dirname $(dirname $(realpath $(which javac))))"
 
+# Podman: GPU Support
+export CONTAINERS_MACHINE_PROVIDER=libkrun
+export DOCKER_HOST=unix:///var/run/docker.sock
+
 export HOMEBREW_NO_ANALYTICS=1
