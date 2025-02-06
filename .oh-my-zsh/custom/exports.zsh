c805ad2ace6b63d1f6b381a3eda537a8878ddf25
James H. Nguyen <james.nguyen@flyr.com>
2025-02-05 18:12:58 -0800

    Configure k9s

--- .oh-my-zsh/custom/exports.zsh
+++ .oh-my-zsh/custom/exports.zsh
@@ -8,4 +8,5 @@ export JAVA_HOME="$(dirname $(dirname $(realpath $(which javac))))"
 export CONTAINERS_MACHINE_PROVIDER=libkrun
 export DOCKER_HOST=unix:///var/run/docker.sock
 
+export XDG_CONFIG_HOME=~/.config
 export HOMEBREW_NO_ANALYTICS=1
