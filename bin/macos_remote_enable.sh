dc37f00fed1a5d8dfc1cd12bd9cb8e939db779f7
James H. Nguyen <james.nguyen@flyr.com>
2025-02-20 19:18:59 -0800

    Fix script

--- bin/macos_remote_enable.sh
+++ bin/macos_remote_enable.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+sudo systemsetup -getremotelogin
 sudo systemsetup -setremotelogin on
 #sudo dseditgroup -o edit -a james -t group com.apple.access_ssh
+#dseditgroup -o create -q james -t user com.apple.access_ssh
+sudo dseditgroup -o edit -a ${USER} -t user com.apple.access_ssh
 sudo /System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart -activate -configure -access -on -users admin,james -privs -all -restart -agent -menu
+sudo systemsetup -getremotelogin
