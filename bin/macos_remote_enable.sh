7a57722537d9ea6f9471fa7218ad31cb2ce1f05e
James H. Nguyen <james.nguyen@flyr.com>
2025-02-20 18:48:55 -0800

    Update remote user script

--- bin/macos_remote_enable.sh
+++ bin/macos_remote_enable.sh
@@ -2,4 +2,4 @@
 
 sudo systemsetup -setremotelogin on
 #sudo dseditgroup -o edit -a james -t group com.apple.access_ssh
-sudo /System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart -activate -configure -access -on -users admin -privs -all -restart -agent -menu
+sudo /System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart -activate -configure -access -on -users admin,james -privs -all -restart -agent -menu
